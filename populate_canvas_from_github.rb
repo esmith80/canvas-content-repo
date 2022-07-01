e17f5254ea13b963033d4ef2eb9a653df9578fbf
Edward Smith <65724195+esmith80@users.noreply.github.com>
2022-07-01 10:24:02 -0600

    fix lesson naming method

--- populate_canvas_from_github.rb
+++ populate_canvas_from_github.rb
@@ -1,15 +1,18 @@
 require 'cgi'
 
 def create_lessons
-  puts "Enter a filename in the current directory that contains a list of github URLs: "
-  file_path = gets.chomp
-  puts "Enter the course ID (int):"
-  course_id = gets.chomp
+  # puts "Enter a filename in the current directory that contains a list of github URLs: "
+  # file_path = gets.chomp
+  # puts "Enter the course ID (int):"
+  # course_id = gets.chomp
+
+  file_path = 'short_filenames.txt'
+  course_id = 3
 
   File.open(file_path.to_s, 'r') do |f|
     f.each_line do |path|  
       @path = clean_path(path)
-      cmd = "github-to-canvas --create-from-github #{@path} --course #{course_id} --type #{lesson_type} --name #{file_name} -lr --forkable"
+      cmd = "github-to-canvas --create-from-github #{@path} --course #{course_id} --type #{lesson_type} --name #{lesson_name} -lr --forkable"
       system(cmd)
     end
   end
@@ -23,19 +26,23 @@ def lesson_type
     end
 end
 
-def file_name
+def lesson_name
     path_parts = @path.split('/')
     path_parts = path_parts[-1].split('__')
     # this string is run by a shell, so it should use escaped characters (`\ ` for space, etc.)
-    path_parts[-2].gsub('%20', '\ ')
+    name = path_parts[-2].gsub('%20', '\ ')
+    name = name.gsub(/%3F/, '?')
+    # HACKY - turn 's in to "is a" having a single quote in the cmd is causing grief (isn't there a better way?)
+    name = name.gsub(/%27s/, '\ is\ a')
 end
 
 def clean_path(path)
   path = path.sub(/\n/,'')
   path = URI.escape(path)
-  # URI.escape doesn't remove single quotes or question marks
+  # URI.escape doesn't remove single quotes or question marks, if single quote, shell won't run command properly
   path = path.gsub(/'/, '%27')
-  path.gsub(/\?/, '%3F')
+  # if question mark, url can't be read properly
+  path = path.gsub(/\?/, '%3F')
 end
 
 
